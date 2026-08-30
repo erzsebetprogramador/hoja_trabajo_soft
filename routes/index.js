7ab16f22d137f3fe6516b68d10dd354f6da412da
Elizabeth Caxaj <125336145+erzsebetprogramador@users.noreply.github.com>
2026-08-29 22:33:45 -0600

    Add verification routes to the main router

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const crearUsuarioRoutes = require("./usuarioRoutes");
 const crearConexionRoutes = require("./conexionRoutes");
+const crearVerificacionRoutes = require("./verificacionRoutes");
 
 function registrarRutas({ usuarioService, db }) {
     const router = express.Router();
 
     router.use(crearUsuarioRoutes(usuarioService));
     router.use(crearConexionRoutes(db));
+    router.use(crearVerificacionRoutes(usuarioService));
 
     return router;
 }
